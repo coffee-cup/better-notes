fde4f50458e4bf96161017b7a767b5a6b48cd480
Jake Runzer <jakerunzer@gmail.com>
2017-07-19 23:14:42 -0700

    user controller and error view test

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -9,14 +9,10 @@ defmodule BetterNotes.UserControllerTest do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, user_path(conn, :index)
-    assert json_response(conn, 200) == []
-  end
-
   test "shows chosen resource", %{conn: conn} do
     user = Repo.insert! %User{}
-    conn = get conn, user_path(conn, :show, user)
+    conn = guardian_login(conn, user)
+    conn = get conn, user_path(conn, :show)
     assert json_response(conn, 200) == %{"id" => user.id,
       "email" => user.email,
       "auth_provider" => user.auth_provider,
@@ -25,40 +21,15 @@ defmodule BetterNotes.UserControllerTest do
       "avatar" => user.avatar}
   end
 
-  test "renders page not found when id is nonexistent", %{conn: conn} do
-    assert_error_sent 404, fn ->
-      get conn, user_path(conn, :show, -1)
-    end
+  test "unauthorized error when user is not logged in", %{conn: conn} do
+    conn = get conn, user_path(conn, :show)
+    assert conn.status == 401
   end
 
-  test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, user_path(conn, :create), user: @valid_attrs
-    assert json_response(conn, 201)["id"]
-    assert Repo.get_by(User, @valid_attrs)
-  end
-
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, user_path(conn, :create), user: @invalid_attrs
-    assert json_response(conn, 422)["errors"] != %{}
-  end
-
-  test "updates and renders chosen resource when data is valid", %{conn: conn} do
-    user = Repo.insert! %User{}
-    conn = put conn, user_path(conn, :update, user), user: @valid_attrs
-    assert json_response(conn, 200)["id"]
-    assert Repo.get_by(User, @valid_attrs)
-  end
-
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    user = Repo.insert! %User{}
-    conn = put conn, user_path(conn, :update, user), user: @invalid_attrs
-    assert json_response(conn, 422)["errors"] != %{}
-  end
-
-  test "deletes chosen resource", %{conn: conn} do
-    user = Repo.insert! %User{}
-    conn = delete conn, user_path(conn, :delete, user)
-    assert response(conn, 204)
-    refute Repo.get(User, user.id)
+  test "unauthorized error when user has not been created", %{conn: conn} do
+    user = User.changeset %User{}, @valid_attrs
+    conn = guardian_login(conn, user)
+    conn = get conn, user_path(conn, :show)
+    assert conn.status == 401
   end
 end
--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -1,21 +1,20 @@
 defmodule BetterNotes.ErrorViewTest do
   use BetterNotes.ConnCase, async: true
 
-  # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(BetterNotes.ErrorView, "404.html", []) ==
-           "Page not found"
+  test "renders 404.json" do
+    assert render(BetterNotes.ErrorView, "404.json", []) ==
+      %{title: "Resource not found", code: 404}
   end
 
-  test "render 500.html" do
-    assert render_to_string(BetterNotes.ErrorView, "500.html", []) ==
-           "Internal server error"
+  test "render 500.json" do
+    assert render(BetterNotes.ErrorView, "500.json", []) ==
+      %{title: "Internal Server Error", code: 500}
   end
 
   test "render any other" do
-    assert render_to_string(BetterNotes.ErrorView, "505.html", []) ==
-           "Internal server error"
+    assert render(BetterNotes.ErrorView, "505.json", []) ==
+      %{title: "Internal Server Error", code: 500}
   end
 end
