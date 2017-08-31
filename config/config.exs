77d063677c65e245c75197011aef65c64f11d688
Jake Runzer <jakerunzer@gmail.com>
2017-08-30 21:39:05 -0700

    fix callback url'

--- config/config.exs
+++ config/config.exs
@@ -34,7 +34,7 @@ config :ueberauth, Ueberauth,
 config :ueberauth, Ueberauth.Strategy.Google.OAuth,
   client_id: System.get_env("GOOGLE_CLIENT_ID"),
   client_secret: System.get_env("GOOGLE_CLIENT_SECRET"),
-  redirect_uri: System.get_env("GOOGLE_REDIRECT_URI")
+  redirect_uri: System.get_env("GOOGLE_CALLBACK_URL")
 
 # Guardian configuration
 config :guardian, Guardian,
